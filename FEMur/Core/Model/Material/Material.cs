9292cc2d7a45627332f7c9f6ff521eb22297fb77
hrdsh <hrdsh.std@gmail.com>
2025-02-08 06:55:40 +0900

    プロジェクト ファイルを追加します。

---
+++ FEMur/Core/Model/Material/Material.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FEMur.Core.Model.Material
+{
+    internal class Material
+    {
+        int id { get; } = 0;
+        string name { get; } = string.Empty;
+        double E { get; } = 0;
+        double nu { get; } = 0;
+
+        public Material(int id,string name , double E , double nu ) { 
+            
+            this.id = id;
+            this.name = name;
+            this.E = E;
+            this.nu = nu;
+
+        }
+
+        public override string ToString()
+        {
+            return $"Material {this.id}: Name {this.name}, E {this.E}, NU {this.nu}";
+        }
+
+    }
+}
---
+++ FEMur/Core/Model/Section/Section.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FEMur.Core.Model.Section
+{
+    internal class Section
+    {
+        int id { get; } = 0;
+        double thickness { get; } = double.NaN;
+        public Section(int id , double thickness) { 
+            this.id = id;  
+            this.thickness = thickness;
+        }
+
+        public override string ToString()
+        {
+            return $"Section {this.id}: thickness {this.thickness}";
+        }
+    }
+}
